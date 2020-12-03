f7bb4e925082939329aad6e19887a31efe448e60
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-12-03 15:59:14 +0800

    feat: ref & borrow & slice

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 mod fibonacci;
 mod temperature_convert;
+mod own;
 
 fn main() {
   let args = env::args().collect::<Vec<String>>();
@@ -8,12 +9,15 @@ fn main() {
   if args.len() < 2 {
     return println!("[error] no key was given.")
   }
+
+
   
   let note_key = args[1].parse::<usize>().unwrap();
 
   match note_key {
     1 => fibonacci::start(),
     2 => temperature_convert::start(),
+    3 => own::start(),
     _ => println!("[warn] give a right key for note start.")
   }
 }
---
+++ src/own.rs
@@ -0,0 +1,69 @@
+// use std::collections::HashMap;
+
+// pub fn start() {
+//   let map : HashMap<&str, String> = HashMap::new();
+
+//   take(&map);
+
+//   println!("{}", map.len());
+// }
+
+// fn take(map: &HashMap<&str, String>) {
+//   // do somthing
+// }
+
+pub fn start() {
+
+  // 可行
+  let mut a = String::from("123");
+
+  println!("{}", a);
+
+  let c = &mut a;
+
+  println!("{}", c);
+
+  let b = &a;
+
+  println!("{}", b);
+
+
+  // 可行
+
+  let d = &mut a;
+
+  println!("{}", d);
+
+  let e = &mut a;
+
+  println!("{}", e);
+
+  println!("{}", e);
+
+  // 不可行
+
+  // let f = &mut a;
+  // let h = &mut a;
+
+  // println!("{}, {}", f, h);
+
+  let s = "Love and Peace";
+
+  let word = first_word(&s[..]);
+
+  println!("{}", s);
+
+  println!("{}", word);
+}
+
+fn first_word(s: &str) -> &str {
+  
+  
+  for (i,char) in s.chars().enumerate() {
+    if char == ' ' {
+      return &s[..i];
+    }
+  };
+
+  return &s;
+}
