4f15b2ee2984640720b74a41c7c72eea6884febc
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-12-12 23:09:42 +0800

    feat: vec owner test

--- src/modules/vector_test.rs
+++ src/modules/vector_test.rs
@@ -1,4 +1,3 @@
-
 pub fn start() {
   let s = String::from("1");
   let mut v = vec![s, String::from("2")];
@@ -13,6 +12,31 @@ pub fn start() {
     println!("{}", d);
   }
 
+  let string1 = "string";
+  let string2 = "string2";
 
-  
+  {
+    let mut str_vec = vec![string1, string2];
+
+    let a = str_vec[0];
+    str_vec.push("string");
+    println!("{}", a);
+  }
+
+  println!("{}", string2);
+
+  let string3 = String::from("hahah");
+  let string4 = String::from("ggag");
+  let string5 = String::from("sgasgd");
+
+  {
+    let mut str_vec = vec![string3, string4];
+
+    let a = &str_vec[0];
+    println!("{}", a);
+    str_vec.push(string5);
+  }
+
+  // now string3, string4 and string5 had dropped...
+  // println!("{}", string3); painc
 }
